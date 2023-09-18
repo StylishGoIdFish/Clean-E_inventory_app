446ab7cc6b0012d848ceb7d4dd7e839bcd1702e3
StylishGoldFish <1solomonall@gmail.com>
2023-09-18 17:11:31 -0400

    Adding the menu files

--- sourceFiles/saveAndTakeData.java
+++ sourceFiles/saveAndTakeData.java
@@ -1,3 +1,34 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class saveAndTakeData {
+    private final File fileName = new File("DATA.csv");;
+    private Scanner reader;
+
+    public void takeDataUsingID(String IDChoice){
+        ArrayList<String[]> returnEntries = new ArrayList<>();
+
+        try {
+            reader = new Scanner(fileName);
+
+            while (reader.hasNextLine()){
+                String fullRow = reader.nextLine();
+                String[] rowContents = fullRow.split(",");
+                if (rowContents[1].equals(IDChoice)){
+                    returnEntries.add(rowContents);
+                }
+            }
+            reader.close();
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.print("Wahhhhhh :'( ");
+            e.printStackTrace();
+        }
+
+    }
+
+
 
 }
