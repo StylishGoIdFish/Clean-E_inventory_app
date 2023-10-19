dc3c326ee83bcd6f03bb6a61a22c178826451980
StylishGoldFish <1solomonall@gmail.com>
2023-10-19 16:49:29 -0400

    Fixing some warnings!

--- sourceFiles/InventoryInfoTab.java
+++ sourceFiles/InventoryInfoTab.java
@@ -1,13 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class InventoryInfoTab extends JPanel {
-    private JTextField textField1, textField2;
-    private JButton buttonOrder, button2;
-    private JTextArea displayArea;
+    private final JTextField textField1;
+    private final JTextField textField2;
+    private final JTextArea displayArea;
 
     saveAndTakeData dataHandler = new saveAndTakeData();
 
@@ -19,11 +18,11 @@ public class InventoryInfoTab extends JPanel {
         JPanel inputPanel = new JPanel(new GridLayout(2, 2));
         textField1 = new JTextField();
         inputPanel.add(textField1);
-        buttonOrder = new JButton("Search For a Specific Entry here using order #");
+        JButton buttonOrder = new JButton("Search For a Specific Entry here using order #");
         inputPanel.add(buttonOrder);
         textField2 = new JTextField();
         inputPanel.add(textField2);
-        button2 = new JButton("Search for a set of all entries for type of object");
+        JButton button2 = new JButton("Search for a set of all entries for type of object");
         inputPanel.add(button2);
 
         displayArea = new JTextArea();
@@ -32,34 +31,30 @@ public class InventoryInfoTab extends JPanel {
         add(inputPanel, BorderLayout.NORTH);
         add(scrollPane, BorderLayout.CENTER);
 
-        buttonOrder.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String text = textField1.getText();
-                try {
-                    String[] entry = (dataHandler.takeDataUsingOrderID(text)).getFirst();
-                    InventoryItem returnItem = new InventoryItem(entry);
-                    displayArea.append("---ORDER FOUND-- " + returnItem.toString() + "\n");
-                }
-                catch (FileNotFoundException error) {
-                    error.printStackTrace();
-                }
-                textField1.setText("");
+        buttonOrder.addActionListener(e -> {
+            String text = textField1.getText();
+            try {
+                String[] entry = (dataHandler.takeDataUsingOrderID(text)).getFirst();
+                InventoryItem returnItem = new InventoryItem(entry);
+                displayArea.append("---ORDER FOUND-- " + returnItem.toString() + "\n");
             }
+            catch (FileNotFoundException error) {
+                error.printStackTrace();
+            }
+            textField1.setText("");
         });
 
-        button2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String text = textField2.getText();
+        button2.addActionListener(e -> {
+            String text = textField2.getText();
 
-                ArrayList<String[]> entries = dataHandler.takeDataUsingType(text);
+            ArrayList<String[]> entries = dataHandler.takeDataUsingType(text);
 
-                for (String[] entry : entries){
-                    InventoryItem returnItem = new InventoryItem(entry);
-                    displayArea.append("---ORDER FOUND--- " + returnItem.toString() + "\n");
-                }
-
-                textField2.setText("");
+            for (String[] entry : entries){
+                InventoryItem returnItem = new InventoryItem(entry);
+                displayArea.append("---ORDER FOUND--- " + returnItem.toString() + "\n");
             }
+
+            textField2.setText("");
         });
     }
 }
